540aa606e84a88538abcae891712ef360b902f93
lucerohospina <lucerojhp@gmail.com>
2018-01-30 01:59:52 -0500

    agregando funcion hover aun en proceso, anade mas comentarios segun preferencia de plugin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 (function($) {
+  // Propuesta de cardify que implementa la etiqueta figure sin estilos
   $.fn.cardify = function() {
     // Ubicando todas etiquetas img del contenedor con la clase cardify-mode
     $(this).find('img').each(function() {
@@ -7,9 +8,20 @@
       // A cada padre de la etiqueta img se le pondra como ultimo hijo una etiqueta figcaption
       // A cada figcaption se le pondra como texto el atributo de su imagen correspondiente
       $(this).parent().append($('<figcaption>' + $(this).attr('alt') + '</figcaption>'));
+      
+      // hover
+      $(this).hover(
+        function() {
+          $(this).css('opacity', '0');
+          $(this).next().css('display', 'none');
+        }, function() {
+          $(this).css('opacity', '1');
+          $(this).next().css('display', 'block');
+        }
+      );
     });
   };
-
+  // Propuesta de estilo inline-block de figure con tamaño de imágenes redimensionado, caption arreglado.
   $.fn.cardifyStylesInline = function() {
     // Ubicando todas etiquetas img del contenedor con la clase cardify-mode
     $(this).find('img').each(function() {
@@ -26,9 +38,19 @@
           'display': 'inline-block'});
       $('figcaption').css({'text-align': 'center',
         'font-size': '1.5em'});
+  
+      // hover
+      // $(this).hover(
+      //   function() {
+      //     var title = $(this).attr('title');
+      //     $(this).css('opacity', '0');
+      //   }, function() {
+      //     $(this).css('opacity', '1');
+      //   }
+      // );
     });
   };  
-
+  // Propuesta con el estilo default de figure con tamaño de imágenes redimensionado, caption arreglado.
   $.fn.cardifyStylesBlock = function() {
     // Ubicando todas etiquetas img del contenedor con la clase cardify-mode
     $(this).find('img').each(function() {
