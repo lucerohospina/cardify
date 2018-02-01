ed41935cb497b5be73eb044860cd601694a10c98
margb13 <marg.b13@hotmail.com>
2018-02-01 03:40:49 -0500

    Realizando cambios al index y editando README

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,11 @@
 'use strict';
 
 // LLAMANDO AL PLUGIN segun su preferencia:
-$(document).ready(function() {
+$(document).ready(function () {
   // $('.cardify-mode').cardify();
   // $('.cardify-mode').cardifyStylesInline();
   // $('.cardify-mode').cardifyStylesBlock();
+  $('#btn-github').on('click', function () {
+    $(location).attr('href', 'https://github.com/margb13/cardify');
+  });
 });
